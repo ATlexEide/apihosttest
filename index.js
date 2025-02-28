bac6d17f8cda4b024d0737da162b9cd741703c48
Alex <atlexeide@gmail.com>
2025-02-28 13:32:16 +0100

    Add test routes

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const { test } = require("./Handlers/GET/getHandler");
+const { test } = require("./Handlers/POST/posttHandler");
+
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.get("/test", test);
+app.post("/test", test);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
