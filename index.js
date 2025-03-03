6f992313488caa1a8bff3673a076f13acfd5c074
Alexander Eide <atlexeide@gmail.com>
2025-03-03 21:51:02 +0100

    Remove unused code

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 import express from "express";
-import * as path from "path";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
 
 import { getRoot, getLogin } from "./Handlers/GET/getHandler.js";
 import { login, addUser } from "./Handlers/POST/postHandler.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 const app = express();
 app.use(express.urlencoded());
 export const router = express.Router();
 const port = 5000;
-
-// const db = new sqlite3.Database("chinook.db", sqlite3.OPEN_READWRITE);
 
 app.get("/", getRoot);
 app.get("/user/", getLogin);
