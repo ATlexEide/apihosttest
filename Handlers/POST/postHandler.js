49c215bf03b7a46af5a61e8a10d5e5d3264005bf
Alexander Eide <atlexeide@gmail.com>
2025-03-03 21:44:29 +0100

    Update handler logic

--- Handlers/POST/postHandler.js
+++ Handlers/POST/postHandler.js
@@ -1,15 +1,39 @@
+import sqlite3 from "sqlite3";
+import * as path from "path";
+
 export function postTest(req, res) {
   console.log("submitted");
-  console.log(req.body.json());
+  console.log(req.body);
+  res.send(req.body);
   // res.send("youve hit post");
 }
 export function addUser(req, res) {
-  console.log(req);
-  const sql = `INSERT INTO users(isOrganizer,first_name,last_name,birthday,country_code,phone,email,pw_hash) VALUES (0,"Alexander","Larsen","31-03-2002","NO",12345678,"testmail2@mail.no","hashforpw");`;
+  console.log(req.body);
+  console.log("name: ", req.body.firstname + " " + req.body.lastname);
+  console.log("phone: ", req.body.phone);
+  console.log("email: ", req.body.email);
+  const sql = `INSERT INTO users(isOrganizer,first_name,last_name,birthday,country_code,phone,email,pw_hash) VALUES (0,"${req.body.firstname}","${req.body.lastname}","31-03-2002","NO",${req.body.phone},"${req.body.email}","hashforpw");`;
   const db = new sqlite3.Database("conventionTest.db", sqlite3.OPEN_READWRITE);
   db.all(sql, async (err, rows) => {
+    if (err) console.log(err);
     console.log(rows);
-    res.send(rows);
+    res.send("Done");
+  });
+  db.close();
+}
+export function login(req, res) {
+  console.log(req.body);
+  console.log("email: ", req.body.email.replace("@", "@"));
+  console.log("password: ", req.body.password);
+  const sql = `SELECT email FROM users WHERE email = ${req.body.email.replace(
+    "@",
+    "_@"
+  )}`;
+  const db = new sqlite3.Database("conventionTest.db", sqlite3.OPEN_READWRITE);
+  db.all(sql, async (err, rows) => {
+    if (err) console.log(err);
+    console.log(rows);
+    res.send("Email exists");
   });
   db.close();
 }
