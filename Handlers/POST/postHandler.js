68957950097cbab306d0e8b51f3c467eb2291b91
Alex <atlexeide@gmail.com>
2025-03-03 16:27:36 +0100

    Sync

--- Handlers/POST/postHandler.js
+++ Handlers/POST/postHandler.js
@@ -1,3 +1,15 @@
 export function postTest(req, res) {
-  res.send("youve hit post");
+  console.log("submitted");
+  console.log(req.body.json());
+  // res.send("youve hit post");
+}
+export function addUser(req, res) {
+  console.log(req);
+  const sql = `INSERT INTO users(isOrganizer,first_name,last_name,birthday,country_code,phone,email,pw_hash) VALUES (0,"Alexander","Larsen","31-03-2002","NO",12345678,"testmail2@mail.no","hashforpw");`;
+  const db = new sqlite3.Database("conventionTest.db", sqlite3.OPEN_READWRITE);
+  db.all(sql, async (err, rows) => {
+    console.log(rows);
+    res.send(rows);
+  });
+  db.close();
 }
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from "express";
+import * as path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = express();
 export const router = express.Router();
-const port = 3000;
+const port = 5000;
 
 import { getTest, dbTest } from "./Handlers/GET/getHandler.js";
 import { postTest } from "./Handlers/POST/postHandler.js";
@@ -9,7 +15,18 @@ import { postTest } from "./Handlers/POST/postHandler.js";
 // const db = new sqlite3.Database("cchinook.db", sqlite3.OPEN_READWRITE);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  const options = {
+    root: path.join(__dirname),
+  };
+
+  const fileName = "index.html";
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      console.error("Error sending file:", err);
+    } else {
+      console.log("Sent:", fileName);
+    }
+  });
 });
 app.get("/test", getTest);
 app.get("/db", dbTest);
