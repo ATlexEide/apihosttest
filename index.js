424ce9ac47d5a980e9f77a6674a36f1072497f13
Alexander Eide <atlexeide@gmail.com>
2025-03-07 10:52:11 +0100

    Add public path

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
+import * as path from "path";
 
 import { getFile } from "./Handlers/getHandler.js";
 import { login, addUser } from "./Handlers/postHandler.js";
 
 const app = express();
 app.use(express.urlencoded());
+app.use(express.static(path.resolve("./") + "/public"));
 export const router = express.Router();
 const port = 5000;
 
