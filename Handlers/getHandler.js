7489a378d446a190d7673089e4fa3a2f117c8c8b
Alexander Eide <atlexeide@gmail.com>
2025-03-07 12:21:30 +0100

    Remove test function

--- Handlers/getHandler.js
+++ Handlers/getHandler.js
@@ -17,13 +17,3 @@ export function getFile(req, res) {
     }
   });
 }
-
-export function dbTest(req, res) {
-  const sql = `SELECT trackid FROM tracks ORDER BY trackid desc AND trackid LIMIT 2`;
-  const db = new sqlite3.Database("chinook.db", sqlite3.OPEN_READWRITE);
-  db.all(sql, async (err, rows) => {
-    console.log(rows);
-    res.send(rows);
-  });
-  db.close();
-}
