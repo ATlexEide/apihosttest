5232f0f7902f35b7def6e05ab003842c42eb96f2
Alexander Eide <atlexeide@gmail.com>
2025-03-07 10:43:04 +0100

    Refactor

--- Handlers/getHandler.js
+++ Handlers/getHandler.js
@@ -1,18 +1,15 @@
 import sqlite3 from "sqlite3";
 import * as path from "path";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-export function getRoot(req, res) {
-  console.log(__dirname);
+export function getFile(req, res) {
+  console.log(req.url);
+  const file = req.url === "/" ? "index" : req.url;
   const options = {
     root: path.resolve("./"),
   };
 
-  const fileName = "index.html";
+  const fileName = `${file}.html`;
+  const css = `style.css`;
   res.sendFile(fileName, options, function (err) {
     if (err) {
       console.error("Error sending file:", err);
@@ -20,21 +17,13 @@ export function getRoot(req, res) {
       console.log("Sent:", fileName);
     }
   });
-}
-
-export function getLogin(req, res) {
-  const options = {
-    root: path.join(__dirname),
-  };
-  const fileName = "/login.html";
-  res.sendFile(fileName, options, function (err) {
+  res.sendFile(css, options, function (err) {
     if (err) {
       console.error("Error sending file:", err);
     } else {
       console.log("Sent:", fileName);
     }
   });
-  res.send("Yippie login");
 }
 
 export function dbTest(req, res) {
